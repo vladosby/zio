8ceff47ccbeda77d4d3d9c567311fb82e823fd75
Uladzislau Kiyeuski <uladzislau_kiyeuski@epam.com>
2020-05-19 21:30:11 +0300

    Init ZIO project

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "zio"
+
+version := "1.0-SNAPSHOT"
+
+scalaVersion := "2.13.1"
+
+libraryDependencies += "dev.zio" %% "zio" % "1.0.0-RC19-2"
+
+libraryDependencies += "dev.zio" %% "zio-streams" % "1.0.0-RC19-2"
---
+++ src/main/scala/com/zio/MyApp.scala
@@ -0,0 +1,16 @@
+package com.zio
+
+import zio.console._
+
+object MyApp extends zio.App {
+
+  def run(args: List[String]) =
+    myAppLogic.fold(_ => 1, _ => 0)
+
+  val myAppLogic =
+    for {
+      _    <- putStrLn("Hello! What is your name?")
+      name <- getStrLn
+      _    <- putStrLn(s"Hello, ${name}, welcome to ZIO!")
+    } yield ()
+}
